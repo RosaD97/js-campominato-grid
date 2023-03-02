f9807aa0c57405fad7b962f4c60fceabad01b1f7
RosaD97 <r960595@gmail.com>
2023-03-02 17:01:18 +0100

    da fixare

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//<div class="cella">1</div>
+
+// FUNZIONI
+function creaCella(elemento, classe){
+    const elementoCreato = document.createElement('elemento');
+    elementoCreato.classList.add(classe);
+    console.log(elemento)
+    return elementoCreato;
+}
+
+function appendiCella(container, element){
+    console.log(container)
+    container.append(element);
+
+}
+
+// MAIN
+
+const container = document.querySelector(".ms_container");
+const cella = creaCella('div', '.cella')
+appendiCella(container, cella);

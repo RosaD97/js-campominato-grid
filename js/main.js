0d0357ce400a9f5e2e37b7b5cabb618fd0c08c29
RosaD97 <r960595@gmail.com>
2023-03-03 10:52:18 +0100

    completato

--- js/main.js
+++ js/main.js
@@ -1,36 +1,56 @@
 'use strict';
 
 // FUNZIONI
-function creaCella(elemento, classe, valore){
+// Crea Cella
+function creaCella(elemento, classe, classeDue, valore){
     const elementoCreato = document.createElement(elemento);
     elementoCreato.classList.add(classe);
+    elementoCreato.classList.add(classeDue);
     elementoCreato.innerText = valore;
 
     elementoCreato.addEventListener('click', function(){
         elementoCreato.classList.add('colors');
         alert(valore);
     })
+
     return elementoCreato;
 }
-
+// Appendi Cella
 function appendiCella(container, element){
     container.append(element);
 }
 
-// MAIN
+// Crea Difficoltà
+function start(){
+    const difficolta = document.querySelector(".ms_label").value;
+    const scelta = difficolta;
+    container.innerHTML = '';
+    
+    if(scelta === 'normal'){
+        for(let i = 1; i <= 81; i++){
+            const cella = creaCella('div', 'cella', 'normal', i);
+            appendiCella(container, cella);
+        }
+    } else if (scelta === 'hard'){
+        for(let i = 1; i <= 49; i++){
+            const cella = creaCella('div', 'cella', 'hard', i);
+            appendiCella(container, cella);
+        }
+    } else {
+        for(let i = 1; i <= 100; i++){
+            const cella = creaCella('div', 'cella', 'easy', i);
+            appendiCella(container, cella);
+        }
+    }
 
-const container = document.querySelector(".ms_container");
-const button = document.querySelector(".ms_btn");
-console.log(button)
-
-// Aggiungi Cella
-for(let i = 1; i <= 100; i++){
-    const cella = creaCella('div', 'cella', i);
-    appendiCella(container, cella);
+    container.classList.add('display');
+    container.classList.remove('d-none'); 
 }
 
+// MAIN
+const container = document.querySelector(".ms_container");
+const button = document.querySelector(".ms_btn");
+
 // Pulsante Play
-button.addEventListener('click', function(){
-    container.classList.add('display');
-    container.classList.remove('d-none');
-})
+button.addEventListener('click', start);
+
